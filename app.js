f84132317a112749f021a81ba3ec111ba5ba4355
Nathaniel Tadesse <nathan@marylandman.myfiosgateway.com>
2022-03-06 23:17:28 -0500

    Fix: Inconsistency in login logic where username and password field entry fail on occassion
    Implementation(s): Navigate to team roster of the most recent season

--- app.js
+++ app.js
@@ -10,10 +10,15 @@ const puppeteer = require('puppeteer');
   const LOGIN_URL = 'https://fantasy.nfl.com/account/sign-in?s=fantasy&returnTo=http%3A%2F%2Ffantasy.nfl.com%2Fmyleagues'
 
   await page.goto(LOGIN_URL, { waitUntil: 'networkidle2' })
-
+ 
   // TODO: Error handling for incorrect username and/or password
-  await page.type(SELECTORS.username, CREDS.username, { delay: 100 })
-  await page.type(SELECTORS.password, CREDS.password, { delay: 100 })
+  let typeUsername = await page.type(SELECTORS.username, CREDS.username, { delay: 50 })
+  let typePassword = await page.type(SELECTORS.password, CREDS.password, { delay: 50 })
+
+  await Promise.all([
+    typeUsername,
+    typePassword
+  ])
 
   await Promise.all([
     page.waitForNavigation(), // The promise resolves after navigation has finished
@@ -25,5 +30,29 @@ const puppeteer = require('puppeteer');
     page.click(SELECTORS.season)
   ])
 
+  // Browser on page 'My Team'
+  // Go to roster of last week of regular season, best represenation of team  
+  // Last week of regular season is Week 14 in developer's league 
+  
+  let rosterLink = await page.evaluate( () => {
+    const href = document.body.querySelector('ul > li.ww-14').firstElementChild.getAttribute('href')
+    return 'https://fantasy.nfl.com/' + href
+  })
+  
+  await Promise.all([
+    rosterLink,
+    page.goto(rosterLink, { waitUnitl: 'networkidle2' }),
+    page.waitForNavigation()
+  ])
+
+
+  // Player Object Model 
+  function Player(name, position, pointsTotal) {
+    this.name = name
+    this.position = position
+    this.pointsTotal = pointsTotal
+  }
+ 
+ 
   await browser.close()
 })()
--- selectors.js
+++ selectors.js
@@ -2,6 +2,6 @@ module.exports = {
   username: '#gigya-loginID-60062076330815260',
   password: '[placeholder="Password *"]',
   button: 'input[class="gigya-input-submit"][data-screenset-roles="instance"]',
-  season: '.nameValue.first.last a'
-
+  season: '.nameValue.first.last a', // Most recent season
+  
 }
