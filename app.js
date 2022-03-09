a876311516b6b5aa058f85d2be3c978acee06fe8
Nathaniel Tadesse <tadesse.nate13@gmail.com>
2022-03-08 23:12:06 -0500

    Implementation(s): Retrieve team's player information. Currently outputs to text file Style: Cleaned main async loop

--- app.js
+++ app.js
@@ -1,20 +1,49 @@
 const CREDS = require('./creds.js')
 const SELECTORS = require('./selectors.js')
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer')
 
 const FANTASY_SITE_URL = 'https://fantasy.nfl.com'
 const LOGIN_URL = 'https://fantasy.nfl.com/account/sign-in?s=fantasy&returnTo=http%3A%2F%2Ffantasy.nfl.com%2Fmyleagues'
 
-// Player Object Model 
-function Player(name, position, pointsTotal) {
+// Player Object Model
+function Player (name, position, pointsTotal) {
   this.name = name
   this.position = position
   this.pointsTotal = pointsTotal
 }
- 
+
+function retrieveNames(page, sel) {
+  return page.$$eval(sel, (table) => {
+      const array = []
+      for (let i = 0; i < table.length; i++) {
+        array.push(table[i].textContent)
+      }
+      return array
+    })
+}
+
+function retrievePositions(page, sel) {
+  return page.$$eval(sel, (table) => {
+    const array = [] 
+    for (let i = 0; i < table.length; i++) {
+      array.push(table[i].textContent.slice(0,2))
+    } 
+    return array 
+  })
+}
+
+function retrieveTotalPoints(page, sel) {
+  return page.$$eval(SELECTORS.statTotal, (table) => {
+    const array = []
+    for (let i = 0; i < table.length; i++) {
+      array.push(table[i].textContent)
+    }
+    return array
+  })
+}
 
 (async () => {
-  'use strict';
+  'use strict'
 
   const browser = await puppeteer.launch({ headless: false })
   const page = await browser.newPage()
@@ -24,30 +53,37 @@ function Player(name, position, pointsTotal) {
     await page.waitForSelector(SELECTORS.username)
     await page.type(SELECTORS.username, CREDS.username, { delay: 50 })
     await page.type(SELECTORS.password, CREDS.password, { delay: 50 })
-
-  } catch(err) {
-      console.log("Failed to login into NFL Fantasy site")
+    await Promise.all([page.waitForNavigation(), page.click(SELECTORS.submit)])
+  } catch (error) {
+    console.log('Failed to login into NFL Fantasy site')
   }
 
-  await Promise.all([
-    page.waitForNavigation(), // The promise resolves after navigation has finished
-    page.click(SELECTORS.button),  // Clicking the link will indirectly cause a navigation
-  ])
-  
   try {
     await page.waitForSelector(SELECTORS.season)
-    const leagueLink = await page.$eval('.teamName.teamId-10', (a) => a.getAttribute('href'))  
+    const leagueLink = await page.$eval(SELECTORS.rosterPage, (a) => a.getAttribute('href'))
     await page.goto(`${FANTASY_SITE_URL}` + `${leagueLink}`, { waitUntil: 'networkidle2' })
-  } catch(err)  {
-      console.log('Failed to navigate to league page.')
+
+    // Go to week that is most representative of team roster
+    // Developer has decided that the last regular season game roster satisfies the above criteria best
+    const rosterLink = await page.$eval('a.w-14', (a) => a.getAttribute('href'))
+    await page.goto(`${FANTASY_SITE_URL}` + `${rosterLink}`, { waitUntil: 'networkidle2' })
+  } catch (error) {
+    console.log('Failed to navigate to team roster page.')
   }
-    
-    
 
+  try {
+    const playerNames = await retrieveNames(page, SELECTORS.playerNameAndInfo[0]) 
+    console.log(playerNames)
 
-   
- 
+    const playerPositions = await retrievePositions(page, SELECTORS.playerNameAndInfo[1]) 
+    console.log(playerPositions)
 
+    const playerTotalPoints = await 
+    console.log(playerTotalPoints)
+
+  } catch (error) {
+    console.log('Failed to get roster information')
+  }
   
   await browser.close()
 })()
--- selectors.js
+++ selectors.js
@@ -1,7 +1,9 @@
 module.exports = {
   username: '#gigya-loginID-60062076330815260',
   password: '[placeholder="Password *"]',
-  button: 'input[class="gigya-input-submit"][data-screenset-roles="instance"]',
-  season: '.nameValue.first.last a' // Most recent season
-
+  submit: 'input[class="gigya-input-submit"][data-screenset-roles="instance"]',
+  season: '.nameValue.first.last a', // Most recent season
+  rosterPage: '.teamName.teamId-10',
+  playerNameAndInfo: ['tbody td.playerNameAndInfo a', 'tbody td.playerNameAndInfo em'],
+  statTotal: 'tbody td.stat.statTotal.numeric.last'
 }
